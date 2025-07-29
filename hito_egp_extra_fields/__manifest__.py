a03cc140220414c77ea619c0f52114dfbfb09c0a
mmarziali <matiasmarziali96@gmail.com>
2025-07-29 10:32:34 -0300

    [ADD] hito_egp_extra_fields

---
+++ hito_egp_extra_fields/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    "name": "Hito EGP Extra Fields",
+    "version": "18.0",
+    "depends": ["product_replenishment_cost"],
+    "category": "inventory",
+    "summary": "Extra fields egp",
+    "data": [
+        'views/product_template.xml',
+    ],
+    "installable": True,
+    "application": False,
+}
