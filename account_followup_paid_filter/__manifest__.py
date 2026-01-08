79eee0cd8c0c4105372268f687fd0cece7677f6e
Francisco Germani <134541679+FranGermani@users.noreply.github.com>
2026-01-08 13:57:50 -0300

    [ADD] Modulo account_followup_paid_filter

---
+++ account_followup_paid_filter/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    'name': 'Account Followup - Filter Paid Invoices',
+    'version': '18.0.1.0.0',
+    'category': 'Accounting',
+    'summary': 'Exclude paid invoices from followup reminders',
+    'author': 'FranG',
+    'license': 'LGPL-3',
+    'depends': ['account_followup'],
+    'data': [],
+    'installable': True,
+}
