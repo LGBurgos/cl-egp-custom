8e41861b5e1d25c7b2a64144369599733c5cebad
Francisco Germani <134541679+FranGermani@users.noreply.github.com>
2026-01-09 15:29:20 -0300

    [ADD] modulo account_followup_rheem_patch

---
+++ account_followup_rheem_patch/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    'name': 'Account Followup - RHEEM S.A. Patch',
+    'version': '18.0.1.0.0',
+    'category': 'Accounting',
+    'summary': 'Parche para excluir facturas pagadas en seguimiento de RHEEM S.A.',
+    'author': 'FranG',
+    'license': 'LGPL-3',
+    'depends': ['account_followup'],
+    'data': [],
+    'installable': True,
+    'application': False,
+}
---
+++ account_followup_rheem_patch/models/__init__.py
@@ -0,0 +1,1 @@
+from . import rheem_patch
